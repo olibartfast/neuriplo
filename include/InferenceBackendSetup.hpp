e42677e8f501db0457eb71fb69473d191f4e392b
Francesco Oliva <olibartfast@gmail.com>
2024-07-13 01:42:05 +0200

    Fixed gpu parameter flag not passed to backends setup

--- include/InferenceBackendSetup.hpp
+++ include/InferenceBackendSetup.hpp
@@ -15,20 +15,20 @@
 #include "OVInfer.hpp"
 #endif
 
-std::unique_ptr<InferenceInterface> setup_inference_engine(const std::string& weights, const std::string& modelConfiguration )
+std::unique_ptr<InferenceInterface> setup_inference_engine(const std::string& weights, const std::string& modelConfiguration , const bool use_gpu)
 {
     #ifdef USE_ONNX_RUNTIME
-    return std::make_unique<ORTInfer>(weights, false); 
+    return std::make_unique<ORTInfer>(weights, use_gpu); 
     #elif USE_LIBTORCH 
-    return std::make_unique<LibtorchInfer>(weights, false); 
+    return std::make_unique<LibtorchInfer>(weights, use_gpu); 
     #elif USE_LIBTENSORFLOW 
-    return std::make_unique<TFDetectionAPI>(weights, false); 
+    return std::make_unique<TFDetectionAPI>(weights, use_gpu); 
     #elif USE_OPENCV_DNN 
     return std::make_unique<OCVDNNInfer>(weights, modelConfiguration); 
     #elif USE_TENSORRT
     return std::make_unique<TRTInfer>(weights); 
     #elif USE_OPENVINO
-    return std::make_unique<OVInfer>("", modelConfiguration, false); 
+    return std::make_unique<OVInfer>("", modelConfiguration, use_gpu); 
     #endif
     return nullptr;
 
