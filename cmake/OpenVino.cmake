cd0b71f8975afc0640474ad4bb59c79cc51763c8
Francesco Oliva <olibartfast@gmail.com>
2025-08-02 04:23:35 +0200

    Update OpenVino.cmake

--- cmake/OpenVino.cmake
+++ cmake/OpenVino.cmake
@@ -14,21 +14,33 @@ if(DEFINED ENV{OPENVINO_DIR})
     # Add library directories
     link_directories("$ENV{OPENVINO_DIR}/runtime/lib/intel64")
     
-    # Add to CMAKE_MODULE_PATH
-    list(APPEND CMAKE_MODULE_PATH "$ENV{OPENVINO_DIR}/runtime/cmake")
+    # Add to CMAKE_PREFIX_PATH
+    list(APPEND CMAKE_PREFIX_PATH "$ENV{OPENVINO_DIR}")
+    
+    message(STATUS "Using OPENVINO_DIR from environment: $ENV{OPENVINO_DIR}")
+    message(STATUS "OpenVINO_DIR set to: ${OpenVINO_DIR}")
+    message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
 else()
     # Set default OpenVINO path
-    set(OpenVINO_DIR "${HOME}/dependencies/${OPENVINO_VERSION}/runtime/cmake")
-    set(InferenceEngine_DIR "${HOME}/dependencies/${OPENVINO_VERSION}/runtime/cmake")
+    set(OPENVINO_INSTALL_DIR "$ENV{HOME}/dependencies/openvino_${OPENVINO_VERSION}")
+    set(OpenVINO_DIR "${OPENVINO_INSTALL_DIR}/runtime/cmake")
+    set(InferenceEngine_DIR "${OPENVINO_INSTALL_DIR}/runtime/cmake")
     
     # Add include directories
-    include_directories("${HOME}/dependencies/${OPENVINO_VERSION}/runtime/include")
+    include_directories("${OPENVINO_INSTALL_DIR}/runtime/include")
     
     # Add library directories
-    link_directories("${HOME}/dependencies/${OPENVINO_VERSION}/runtime/lib/intel64")
+    link_directories("${OPENVINO_INSTALL_DIR}/runtime/lib/intel64")
     
-    # Add to CMAKE_MODULE_PATH
-    list(APPEND CMAKE_MODULE_PATH "${HOME}/dependencies/${OPENVINO_VERSION}/runtime/cmake")
+    # Add to CMAKE_PREFIX_PATH
+    list(APPEND CMAKE_PREFIX_PATH "${OPENVINO_INSTALL_DIR}")
+    
+    message(STATUS "Using default OpenVINO path")
+    message(STATUS "OPENVINO_VERSION: ${OPENVINO_VERSION}")
+    message(STATUS "HOME: $ENV{HOME}")
+    message(STATUS "OPENVINO_INSTALL_DIR: ${OPENVINO_INSTALL_DIR}")
+    message(STATUS "OpenVINO_DIR set to: ${OpenVINO_DIR}")
+    message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
 endif()
 
 find_package(OpenVINO REQUIRED)
